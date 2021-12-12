ae9d2d55cdea267dc1e7df2d8cb6adf7325b4032
Joyce <joo20423@gmail.com>
2021-12-12 21:04:20 +0800

    feat:resume頁 按鈕還沒修好

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -3,18 +3,43 @@
     <!-- top -->
     <section class="container-1100">
       <div class="d-flex pt-120 pb-80">
-        <div class="col-25">
-          <img class="w-90" src="../../assets/resume/photo.svg" alt="個人照" />
+        <div class="col">
+          <img class="w-90" src="../../assets/resume/photo.jpg" alt="個人照" />
         </div>
-        <div class="col pl-40">
-          <div class="d-flex flex-align-baseline">
-            <h1 class="fz-40-b gray1">侯佳君</h1>
-            <h3 class="fz-22-m gray1 pl-20">HOU CHIA CHUN</h3>
+        <div class="d-flex-c col-5 pl-20 flex-justify-space-between">
+          <div class="">
+            <div class="d-flex flex-align-baseline">
+              <h1 class="fz-40-b gray1">侯佳君</h1>
+              <h3 class="fz-22-m gray1 pl-20">HOU CHIA CHUN</h3>
+            </div>
+            <p class="fz-16-l gray2 pt-16">Visual &amp; Web Designer</p>
           </div>
-          <p class="fz-16-l gray2 pt-16">Visual &amp; Web Designer</p>
           <h3 class="fz-16-l gray2">
-            原生廣告設計｜HTML、CSS切版｜平面設覺設計｜插畫設計
+            網頁 UI 設計｜HTML、CSS 切版｜廣告平面設計｜插畫設計
           </h3>
+        </div>
+        <div class="d-flex col-3 flex-justify-end">
+          <ElementBtn href="/resume" title="下載履歷" :class="'ml-24'">
+              <span class="fz-14-r-10"
+                >下載履歷
+                <img
+                  class="ml-2"
+                  src="../../assets/icons/ic-download.svg"
+                  alt="關於我"
+              /></span>
+            </ElementBtn>
+          <div class="d-flex flex-align-center">
+            <div class="col-auto fz-16-l gray1 mr-20">0958-680-652</div>
+            <img class="icon" src="../../assets/icons/ic-phone.svg" alt="" />            
+          </div>
+          <div class="d-flex flex-align-center">
+            <div class="col-auto fz-16-l gray1 mr-20">joo20423@gmail.com</div>
+            <img class="icon" src="../../assets/icons/ic-mail.svg" alt="" />            
+          </div>
+          <div class="d-flex flex-align-center">
+            <div class="col-auto fz-16-l gray1 mr-20">https://joyce.tinycloud.tw/</div>
+            <img class="icon" src="../../assets/icons/ic-website.svg" alt="" />            
+          </div>
         </div>
       </div>
     </section>
@@ -22,12 +47,13 @@
     <!-- introduce -->
     <section class="container-1100 pt-80 pb-80 border-top-gray3">
       <p class="fz-16-l gray2">
-        擁有網頁視覺設計師 2 年的經驗，會針對不同事業體、活動，
-        製作出相對應風格視覺的廣告素材， 任職期間運用公司資源上完 RWD
-        課程，精進網頁撰寫能力。 對於介面設計有興趣，自學 UI/UX 知識， Figma
-        操作， 並自我練習透過 Side Project 來實踐所學，
-        並與工程師一起參與相關競賽。<br />
-        我認為設計應站在使用者角度並兼顧產品一致性與易用性，為用戶帶來更棒的體驗。
+        擁有網頁視覺設計師 2
+        年經驗，針對不同事業體、活動，製作出對應風格的廣告素材，任職期間運用公司資源學
+        RWD，精進網頁撰寫能力。在這過程當中，對介面設計產生興趣，因此額外自學
+        UI/UX ，熟悉 Figma 操作，並透過 Side Project 「台鐵 e 訂通 App
+        Redesign」來自我精進，也與工程師一起參與由六角學院舉辦的「第三屆 The F2E
+        前端 &amp; UI 修煉精神時光屋」。
+        <br />我相信設計應站在使用者角度並兼顧產品一致性與易用性，為用戶帶來更棒的體驗。
       </p>
     </section>
 
@@ -39,16 +65,16 @@
           <div class="col border-top-gray3"></div>
         </div>
         <!-- 聯成電腦 -->
-        <p class="fz-16-l gray2">Ｍar. 2020 - Present</p>
-        <h3 class="fz-22-m gray1 pt-12">聯成電腦 Web Visual Designer</h3>
+        <h3 class="fz-22-m gray1">聯成電腦 Web Visual Designer</h3>
+        <p class="fz-16-l gray2 pt-12">Ｍar. 2020 - Present</p>
         <ul class="fz-16-l gray2 pt-16 pb-60">
           <ElementStarItem>
             <template #title>廣告設計</template>
-            負責檔期活動視覺及相關廣告素材，提高近 5 倍績效成長量
+            負責檔期活動視覺及相關廣告素材，提高 5 倍績效
           </ElementStarItem>
           <ElementStarItem>
             <template #title>平面設計</template>
-            跨部門溝通支援平面需求，其由疫情為發想設計的學員年鑑受同仁好評
+            跨部門溝通支援平面需求，其中學員年鑑受同仁好評
           </ElementStarItem>
           <ElementStarItem>
             <template #title>插畫設計</template>
@@ -65,8 +91,8 @@
         </ul>
 
         <!-- 開店123 -->
-        <p class="fz-16-l gray2">Oct. 2019 - Jan. 2020</p>
-        <h3 class="fz-22-m gray1 pt-12">開店 123 Design Assistant</h3>
+        <h3 class="fz-22-m gray1">開店 123 Design Assistant</h3>
+        <p class="fz-16-l gray2 pt-12">Oct. 2019 - Jan. 2020</p>
         <ul class="star-list fz-16-l gray2 pt-16 pb-60">
           <ElementStarItem>
             <template #title>平面設計</template>
@@ -86,7 +112,7 @@
         <ul class="fz-16-l gray2 pt-16 pb-60">
           <ElementStarItem>
             <template #title>Hi BUS!</template>
-            2021 六角學院 The F2E 3rd<br />
+            2021 第三屆 The F2E 前端 &amp; UI 修煉精神時光屋 /<br />
             <a
               class="blue1"
               href="https://hi-bus.tinycloud.tw/"
@@ -98,9 +124,9 @@
           <ElementStarItem>
             <template #title>TAICHUNＧ</template>
             <ul>
-              <li>2019 德國紅點設計｜品牌暨傳達設計紅點獎</li>
-              <li>2019 泰達盃青年創意設計大賽｜入圍</li>
-              <li>2019 放視大賞｜平面傳達設計組入圍</li>
+              <li>2019 德國紅點設計 / 品牌暨傳達設計紅點獎</li>
+              <li>2019 泰達盃青年創意設計大賽 / 入圍</li>
+              <li>2019 放視大賞 - 平面傳達設計組 / 入圍</li>
             </ul>
             <a
               class="blue1"
@@ -116,14 +142,12 @@
           <h2 class="col-auto fz-30-b gray1 pr-24">學歷</h2>
           <div class="col line border-top-gray3"></div>
         </div>
-        <p class="fz-16-l gray2">2015 - 2019</p>
-        <h3 class="fz-22-m gray1 pt-12">
-          亞洲大學 Visual Communication Design
-        </h3>
+        <h3 class="fz-22-m gray1">亞洲大學 Visual Communication Design</h3>
+        <p class="fz-16-l gray2 pt-12">2015 - 2019</p>
         <ul class="fz-16-l gray2 pt-16 pb-60">
           <ElementStarItem>
             <template #title>畢業製作</template>
-            擔任主要負責人，負責項目：色彩規劃、插畫設計、時間規劃、作品導覽
+            擔任主要負責人<br />負責項目：色彩規劃、插畫設計、時間規劃、作品導覽
           </ElementStarItem>
         </ul>
       </div>
@@ -134,27 +158,43 @@
           <h2 class="col-auto fz-30-b gray1 pr-24">專長技能</h2>
           <div class="col line border-top-gray3"></div>
         </div>
-        <h3 class="fz-22-m gray1 pt-12">網頁UI設計</h3>
+        <!-- UI 設計 -->
+        <h3 class="fz-22-m gray1">UI 設計</h3>
+        <p class="fz-16-l gray2 pt-12">Figma</p>
         <ul class="fz-16-l gray2 pt-16 pb-60">
+          <ElementStarItem> 快速手繪、使用軟體製作 Wireframe </ElementStarItem>
+          <ElementStarItem> 制定可重用 Design System </ElementStarItem>
           <ElementStarItem>
-            <template #title>&#8203;</template>
-            具備 UI/UX 設計相關知識
-          </ElementStarItem>
-          <ElementStarItem>
-            <template #title>&#8203;</template>
-            快速手繪、使用軟體製作 Wireframe
-          </ElementStarItem>
-          <ElementStarItem>
-            <template #title>&#8203;</template>
-            了解 Design System 的重要性
-          </ElementStarItem>
-          <ElementStarItem>
-            <template #title>&#8203;</template>
             製作靜態 Mockup 及可互動的 Prototype
           </ElementStarItem>
+        </ul>
+        <!-- 前端概念 -->
+        <h3 class="fz-22-m gray1">前端概念</h3>
+        <p class="fz-16-l gray2 pt-12">Visual Studio Code</p>
+        <ul class="fz-16-l gray2 pt-16 pb-60">
           <ElementStarItem>
-            <template #title>&#8203;</template>
             使用 HTML、CSS 切版完成 Landing Page，具備 RWD 知識
+          </ElementStarItem>
+        </ul>
+        <!-- 平面設計 -->
+        <h3 class="fz-22-m gray1">平面設計</h3>
+        <p class="fz-16-l gray2 pt-12">Illustrator、Photoshop、InDesign</p>
+        <ul class="fz-16-l gray2 pt-16 pb-60">
+          <ElementStarItem> 原生廣告、Banner、EDM設計 </ElementStarItem>
+          <ElementStarItem> Icon、企業識別設計 </ElementStarItem>
+          <ElementStarItem>
+            手繪插畫、影像合成、名片、年鑑、DM設計
+          </ElementStarItem>
+        </ul>
+        <!-- 管理協作 -->
+        <h3 class="fz-22-m gray1">管理協作</h3>
+        <p class="fz-16-l gray2 pt-12">Notion、 Trello、Google Docs</p>
+        <ul class="fz-16-l gray2 pt-16 pb-60">
+          <ElementStarItem>
+            運用 Trello 進行工作上的專案時程管理
+          </ElementStarItem>
+          <ElementStarItem>
+            利用 Notion 設定目標並將資料歸納整理
           </ElementStarItem>
         </ul>
       </div>
