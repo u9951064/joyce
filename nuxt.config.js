cb444a313a24c83ea7087f3c8186e1b4e1d8cd36
Josh Tsai <josh@intentarget.com>
2021-12-16 09:00:45 +0800

    pref: fix Lighthous issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,6 +46,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extractCSS: true,
+    babel: {
+      presets: ['es2015', 'stage-0']
+    }
   },
 
   styleResources: {
