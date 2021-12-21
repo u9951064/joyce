394cc08001e0c46636d780a765495ac8212332dd
Josh Tsai <josh@intentarget.com>
2021-12-21 16:20:42 +0800

    feat: add on scroll behavior

---
+++ app/router.scrollBehavior.js
@@ -0,0 +1,47 @@
+export default function (to, from, savedPosition) {
+  // if the returned position is falsy or an empty object,
+  // will retain current scroll position.
+  let position = { x: 0, y: 0 };
+
+  if(/^works-categories-/.test(to.name) && /^works-categories-/.test(from.name)) {
+    position = false;
+  }
+  console.error(to, from)
+
+  // savedPosition is only available for popstate navigations (back button)
+  if (savedPosition) {
+    position = savedPosition;
+  }
+
+  return new Promise(resolve => {
+    // wait for the out transition to complete (if necessary)
+    window.$nuxt.$once('triggerScroll', () => {
+      // coords will be used if no selector is provided,
+      // or if the selector didn't match any element.
+      if (to.hash) {
+        let hash = to.hash
+        // CSS.escape() is not supported with IE and Edge.
+        if (
+          typeof window.CSS !== 'undefined' &&
+          typeof window.CSS.escape !== 'undefined'
+        ) {
+          hash = '#' + window.CSS.escape(hash.substr(1))
+        }
+        try {
+          if (document.querySelector(hash)) {
+            // scroll to anchor by returning the selector
+            position = { selector: hash }
+          }
+        } catch (e) {
+          console.warn(
+            'Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).'
+          )
+        }
+      }
+      if(position) {
+        position.behavior = 'smooth';
+      }
+      resolve(position);
+    })
+  })
+}
